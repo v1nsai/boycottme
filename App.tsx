97e6a779f68ccdfe241081709da0795224328922
Dr Ew <doctor_ew@protonmail.com>
2021-12-23 14:28:56 -0500

    expo tabbed template and camera working in tab

--- App.tsx
+++ App.tsx
@@ -1,33 +1,22 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { DarkTheme, NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-function ScannerScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Scanner goes here</Text>
-    </View>
-  );
-}
-
-function InfoScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Company info goes here</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
 export default function App() {
-  return (
-    <NavigationContainer theme={DarkTheme}>
-      <Tab.Navigator>
-        <Tab.Screen name="Scanner" component={ScannerScreen} />
-        <Tab.Screen name="Info" component={InfoScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
 }
